c4424f58a594893292f4be8fb2dba336a03bbb60
Alexander Grebenkov <aleksander.grebenkov@saritasa.com>
2021-01-29 17:53:02 +0500

    Track settings

--- Atlas.Mvvm/ViewModels/Settings/TrackSettings/TrackSettingsModel.cs
+++ Atlas.Mvvm/ViewModels/Settings/TrackSettings/TrackSettingsModel.cs
@@ -1,6 +1,13 @@
-﻿namespace Atlas.Mvvm.ViewModels.Settings.TrackSettings
+﻿using System;
+
+namespace Atlas.Mvvm.ViewModels.Settings.TrackSettings
 {
     public class TrackSettingsModel
     {
+        public string City { get; set; }
+        public string Name { get; set; }
+        public int SlotsCount { get; set; }
+        public double TrackLength { get; set; }
+        public TimeSpan DelayAfterStopping { get; set; }
     }
 }
--- Atlas.Mvvm/ViewModels/Settings/TrackSettings/TrackSettingsViewModel.cs
+++ Atlas.Mvvm/ViewModels/Settings/TrackSettings/TrackSettingsViewModel.cs
@@ -1,7 +1,26 @@
-﻿namespace Atlas.Mvvm.ViewModels.Settings.TrackSettings
+﻿using Atlas.Mvvm.Helpers;
+using Atlas.Mvvm.ServiceAbstractions;
+
+namespace Atlas.Mvvm.ViewModels.Settings.TrackSettings
 {
     public class TrackSettingsViewModel : BaseViewModel
     {
+        private readonly INavigationService navigationService;
         public override string Title => "Параметры трассы";
+        public TrackSettingsModel Model { get; }
+        public RelayCommand SaveCommand { get; }
+
+        public TrackSettingsViewModel(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+            Model = new TrackSettingsModel(); // TODO: Load from settings
+            SaveCommand = new RelayCommand(SaveExecute);
+        }
+
+        private void SaveExecute()
+        {
+            // TODO: Do save
+            navigationService.Pop();
+        }
     }
 }
